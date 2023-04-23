fc85c617af4ad73556b3ffcb7c5bcb4f206b15a7
DobleTreinta <anthonyovie15gmail.com>
2023-04-23 00:11:36 -0500

    Creando los paquetes

---
+++ src/main/java/pe/edu/upao/apidelivery/ApiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.apidelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upao/apidelivery/ApiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.apidelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
